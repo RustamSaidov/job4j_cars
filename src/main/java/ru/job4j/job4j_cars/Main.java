6ec26c665fc9ccf41927144b04a1af2a10df1aad
rusta <rustam_ussr@mail.ru>
2023-01-10 17:55:25 +0300

    1. Техническое задание - проект "АвтоМаг" [#504895]

---
+++ src/main/java/ru/job4j/job4j_cars/Main.java
@@ -0,0 +1,10 @@
+package ru.job4j.job4j_cars;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
