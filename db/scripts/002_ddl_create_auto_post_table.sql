0393e97a0b599e33f6651cb1b998119af479b246
rusta <rustam_ussr@mail.ru>
2023-01-11 08:46:29 +0300

    1. Техническое задание - проект "АвтоМаг" [#504895 #379650]

--- db/scripts/002_ddl_create_auto_post_table.sql
+++ db/scripts/002_ddl_create_auto_post_table.sql
@@ -1,9 +1,7 @@
-auto_post(id, description, created, auto_user_id)
-
 CREATE TABLE auto_user_posts
 (
-    id       SERIAL PRIMARY KEY,
-    description    varchar  not null,
+    id SERIAL PRIMARY KEY,
+    description varchar  not null,
     created date,
     auto_user_id int references auto_users(id)
 );
