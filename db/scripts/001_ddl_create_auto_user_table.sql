b3562c42d634ebcb624876e49fa2efafab318c13
rusta <rustam_ussr@mail.ru>
2023-01-12 16:17:30 +0300

    2. Доменная модель проекта "АвтоМаг" [#504897 #379657]

--- db/scripts/001_ddl_create_auto_user_table.sql
+++ db/scripts/001_ddl_create_auto_user_table.sql
@@ -1,6 +1,11 @@
-CREATE TABLE auto_users
+CREATE TABLE auto_user
 (
     id       SERIAL PRIMARY KEY,
     login    varchar unique not null,
     password varchar        not null
 );
+
+comment on table auto_user is 'Таблица автолюбителей';
+comment on table auto_post.id is 'Идентификатор автолюбителя';
+comment on table auto_post.login is 'Логин автолюбителя';
+comment on table auto_post.password is 'Пароль автолюбителя';
