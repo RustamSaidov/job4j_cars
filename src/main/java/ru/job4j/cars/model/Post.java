cc40144a4b25a214a26ba2ca06d31879e5b8a95c
rusta <rustam_ussr@mail.ru>
2023-03-13 18:07:56 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -0,0 +1,28 @@
+package ru.job4j.cars.model;
+
+import javax.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "auto_post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Include
+    private int id;
+
+    private String description;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "auto_post_id")
+    private List<PriceHistory> priceHistories;
+}
